e140a55400e6794d92dd953fddeabc7216c319c3
Ian Reah <ian.reah@nonlinear.com>
2014-01-29 16:20:23 +0000

    Implemented a simple item factory
    It creates placeholder items with a placehold.it url for the image source with an index based label.

---
+++ js/placeholderItemFactory.js
@@ -0,0 +1,11 @@
+﻿define(['placeholderItem'], function(PlaceHolderItem) {
+    return function(itemCount) {
+        this.getItem = function (requestedIndex) {
+            var wrappedIndex = requestedIndex % itemCount;
+            if (wrappedIndex < 0) {
+                wrappedIndex += itemCount;
+            }
+            return new PlaceHolderItem(wrappedIndex);
+        };
+    };
+});
---
+++ specs/placeholderItemFactory.spec.js
@@ -0,0 +1,33 @@
+﻿define(['placeholderItemFactory'], function (ItemFactory) {
+    describe("Place Holder Item Factory module", function () {
+        var itemFactoryUnderTest;
+
+        beforeEach(function () {
+            itemFactoryUnderTest = new ItemFactory(5);
+        });
+
+        it("gets items from a valid index", function () {
+            expect(itemFactoryUnderTest.getItem(0).index()).toBe(0);
+            expect(itemFactoryUnderTest.getItem(1).index()).toBe(1);
+            expect(itemFactoryUnderTest.getItem(2).index()).toBe(2);
+            expect(itemFactoryUnderTest.getItem(3).index()).toBe(3);
+            expect(itemFactoryUnderTest.getItem(4).index()).toBe(4);
+        });
+
+        it("wraps correctly when index requested is higher than the number of items", function () {
+            expect(itemFactoryUnderTest.getItem(5).index()).toBe(0);
+            expect(itemFactoryUnderTest.getItem(6).index()).toBe(1);
+            expect(itemFactoryUnderTest.getItem(42).index()).toBe(2);
+            expect(itemFactoryUnderTest.getItem(58).index()).toBe(3);
+            expect(itemFactoryUnderTest.getItem(79).index()).toBe(4);
+        });
+
+        it("wraps correctly when index requested is negative", function () {
+            expect(itemFactoryUnderTest.getItem(-1).index()).toBe(4);
+            expect(itemFactoryUnderTest.getItem(-2).index()).toBe(3);
+            expect(itemFactoryUnderTest.getItem(-42).index()).toBe(3);
+            expect(itemFactoryUnderTest.getItem(-58).index()).toBe(2);
+            expect(itemFactoryUnderTest.getItem(-79).index()).toBe(1);
+        });
+    });
+});
