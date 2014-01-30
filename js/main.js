597df4f4445d10b46c2a6fff548a824b4d058343
Ian Reah <ian.reah@nonlinear.com>
2014-01-30 09:10:38 +0000

    Use the domReady module...
    ...to make sure the main module function doesn't execute before the page is loaded.
    
    http://requirejs.org/docs/api.html#pageload

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ requirejs.config({
   }
 });
  
-require (['knockout', 'containerViewModel', 'placeholderItemFactory'], function(ko, ContainerViewModel, ItemFactory) {
+require(['knockout', 'containerViewModel', 'placeholderItemFactory', 'requirejs-domready!'], function (ko, ContainerViewModel, ItemFactory, dom) {
     ko.applyBindings(new ContainerViewModel(new ItemFactory(100)));
 
-    /*document.onkeydown = function(event) {
+    /*dom.onkeydown = function(event) {
         switch(event.keyCode)
         {
             case 39: // Right Arrow
