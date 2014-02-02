c36f5f1182e63b1099c9d9008d39f6b5bea3460b
ianreah <ian.reah@gmail.com>
2014-02-02 21:52:05 +0000

    Re-implementing moveNext and movePrevious with ko
    It now updates the items in the container when it has finished sliding
    (drops one of one end, adds a new to the other)
    
    This also required dealing with child items in the container firing
    transition completed events. The solution works on the assumption that
    the child items have their own bindings and viewmodel so I can check
    the ko.dataFor of the event target. (This felt nicer than the view
    model needing to know which element it was getting bound to.)
    
    Also, updated slidingStatus to be a computed property based on the
    direction that the container is moving.
    
    TODO:
    - fading in/out of the items that are getting added/removed
    - setting the current item

--- js/containerViewModel.js
+++ js/containerViewModel.js
@@ -9,18 +9,51 @@ define(['knockout'], function (ko) {
         
         this.items = ko.observableArray(itemsArray);
         this.currentIndex = ko.observable(Math.floor(itemCount/2));
-        this.slidingStatus = ko.observable();
         
+        var direction = ko.observable(0);
+        this.slidingStatus = ko.computed(function() {
+            if(direction() > 0) {
+                return "slide-left";
+            }
+            
+            if(direction() < 0) {
+                return "slide-right";
+            }
+            
+            return "";
+        });
+
         this.moveNext = function() {
-            this.slidingStatus("slide-left");
+            direction(1);
         };
         
         this.movePrevious = function() {
-            this.slidingStatus("slide-right");
+            direction(-1);
         };
         
         this.completeTransition = function(data, event) {
-            this.slidingStatus("");
+            // Ignore events bubbling from the child elements
+            // (Assumes child elements are bound to a different vm)
+            var eventVm = ko.dataFor(event.target);
+            if(eventVm !== this) {
+                return;
+            }
+            
+            var newItemIndex;
+            if(direction() > 0) {
+                newItemIndex = itemsArray[itemCount-1].index() + 1;
+                
+                itemsArray.shift();
+                itemsArray.push(itemFactory.getItem(newItemIndex));
+            } else if (direction() < 0) {
+                newItemIndex = itemsArray[0].index() - 1;
+                
+                itemsArray.pop();
+                itemsArray.unshift(itemFactory.getItem(newItemIndex));
+            }
+            
+            direction(0);
+            this.items.valueHasMutated();
         };
     };
 });
--- specs/containerViewModel.spec.js
+++ specs/containerViewModel.spec.js
@@ -1,4 +1,4 @@
-define(['containerViewModel'], function (ContainerViewModel) {
+define(['containerViewModel', 'knockout'], function (ContainerViewModel, ko) {
     describe("Container View Model module", function () {
         var vmUnderTest;
 
@@ -14,6 +14,10 @@ define(['containerViewModel'], function (ContainerViewModel) {
         
         beforeEach(function () {
             vmUnderTest = new ContainerViewModel(mockItemFactory);
+            
+            ko.cleanNode(document.body);
+            document.body.innerHTML = "<ul id='testContainer' data-bind='foreach: items'><li data-bind='text: index'></li></ul>";
+            ko.applyBindings(vmUnderTest);
         });
         
         it("has 9 items", function () {
@@ -37,7 +41,22 @@ define(['containerViewModel'], function (ContainerViewModel) {
         });
         
         it("isn't sliding yet", function() {
-           expect(vmUnderTest.slidingStatus()).toBeUndefined();
+           expect(vmUnderTest.slidingStatus()).toBe("");
+        });
+        
+        it("ignores completeTransition calls from child elements", function(){
+            var testContainer = document.getElementById("testContainer");
+            vmUnderTest.completeTransition(vmUnderTest, {target: testContainer.children[0]});
+            
+            expect(vmUnderTest.items()[0].index()).toBe(0);
+            expect(vmUnderTest.items()[1].index()).toBe(1);
+            expect(vmUnderTest.items()[2].index()).toBe(2);
+            expect(vmUnderTest.items()[3].index()).toBe(3);
+            expect(vmUnderTest.items()[4].index()).toBe(4);
+            expect(vmUnderTest.items()[5].index()).toBe(5);
+            expect(vmUnderTest.items()[6].index()).toBe(6);
+            expect(vmUnderTest.items()[7].index()).toBe(7);
+            expect(vmUnderTest.items()[8].index()).toBe(8);
         });
         
         describe("moveNext", function() {
@@ -45,8 +64,23 @@ define(['containerViewModel'], function (ContainerViewModel) {
                 vmUnderTest.moveNext();
                 expect(vmUnderTest.slidingStatus()).toBe("slide-left");
                 
-                vmUnderTest.completeTransition();
+                vmUnderTest.completeTransition(vmUnderTest, {target: document.getElementById("testContainer")});
                 expect(vmUnderTest.slidingStatus()).toBe("");
+           });
+           
+           it("updates the items", function(){
+                vmUnderTest.moveNext();
+                vmUnderTest.completeTransition(vmUnderTest, {target: document.getElementById("testContainer")});
+
+                expect(vmUnderTest.items()[0].index()).toBe(1);
+                expect(vmUnderTest.items()[1].index()).toBe(2);
+                expect(vmUnderTest.items()[2].index()).toBe(3);
+                expect(vmUnderTest.items()[3].index()).toBe(4);
+                expect(vmUnderTest.items()[4].index()).toBe(5);
+                expect(vmUnderTest.items()[5].index()).toBe(6);
+                expect(vmUnderTest.items()[6].index()).toBe(7);
+                expect(vmUnderTest.items()[7].index()).toBe(8);
+                expect(vmUnderTest.items()[8].index()).toBe(9);
            });
         });
         
@@ -55,9 +89,24 @@ define(['containerViewModel'], function (ContainerViewModel) {
                 vmUnderTest.movePrevious();
                 expect(vmUnderTest.slidingStatus()).toBe("slide-right");
                 
-                vmUnderTest.completeTransition();
+                vmUnderTest.completeTransition(vmUnderTest, {target: document.getElementById("testContainer")});
                 expect(vmUnderTest.slidingStatus()).toBe("");
-           });
+            });
+                   
+            it("updates the items", function(){
+                vmUnderTest.movePrevious();
+                vmUnderTest.completeTransition(vmUnderTest, {target: document.getElementById("testContainer")});
+                
+                expect(vmUnderTest.items()[0].index()).toBe(-1);
+                expect(vmUnderTest.items()[1].index()).toBe(0);
+                expect(vmUnderTest.items()[2].index()).toBe(1);
+                expect(vmUnderTest.items()[3].index()).toBe(2);
+                expect(vmUnderTest.items()[4].index()).toBe(3);
+                expect(vmUnderTest.items()[5].index()).toBe(4);
+                expect(vmUnderTest.items()[6].index()).toBe(5);
+                expect(vmUnderTest.items()[7].index()).toBe(6);
+                expect(vmUnderTest.items()[8].index()).toBe(7);
+            });
         });
     });
 });
